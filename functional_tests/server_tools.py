79656007731a4f54cc86cbc4b021efeb28c927cd
Bogdan Markov <bogdick@yandex.ru>
2016-07-19 18:00:14 +0300

    Trying

--- functional_tests/server_tools.py
+++ functional_tests/server_tools.py
@@ -1,20 +1,21 @@
 from os import path
 import subprocess
 
-THIS_FOLDER = path.dirname(path.abspath(__file__))
+THIS_FOLDER = path.dirname(path.abspath(__file__)) + '/../deploy_tools'
 
 
 def create_session_on_server(host, email):
     return subprocess.check_output(
         [
             'fab',
-            '-i {}'.format('~/.ssh/BogdanKeyPair.pem'),
+            '-i ~/.ssh/BogdanKeyPair.pem',
             'create_session_on_server:email={}'.format(email),
             '--host={}'.format(host),
             '--hide=everything,status',
         ],
         cwd=THIS_FOLDER
     ).decode().strip()
+
 
 def reset_database(host):
     subprocess.check_call(
