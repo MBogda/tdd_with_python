e9fb7cf648391e2f57270f350345fb8d23827b51
Bogdan Markov <bogdick@yandex.ru>
2016-07-19 15:48:05 +0300

    Update deploying: change 'localhost' to sitename

--- superlists/settings.py
+++ superlists/settings.py
@@ -25,7 +25,10 @@ SECRET_KEY = '_-xf_2)22h2@yt2*q@tfe#tbhh=&p*=ldhvl+7z*0+x0160vr%'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+# This setting is changed by the deploy script
+DOMAIN = "localhost"
+
+ALLOWED_HOSTS = [DOMAIN]
 
 
 # Application definition
@@ -131,3 +134,20 @@ AUTH_USER_MODEL = 'accounts.User'
 AUTHENTICATION_BACKENDS = [
     'accounts.authentication.PersonaAuthenticationBackend',
 ]
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+        },
+    },
+    'root': {'level': 'INFO'},
+}
